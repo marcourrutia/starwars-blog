698470418d2528cdccc9652dd0e88b40ba9ff4b3
marcourrutia <m.urrutia1991@gmail.com>
2024-07-31 18:33:04 -0400

    fondo animado de estrellas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import BackgroundStar from "./components/BackgroundStar";
+import { NavBar } from "./components/NavBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BackgroundStar />
+      <div className="main-container">
+        <NavBar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
